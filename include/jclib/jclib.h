ab8778105005b83058af6ee81a637f54a300b508
Jesus Christ <jesuschrist-official@protonmail.com>
2019-07-11 16:00:57 +0200

    optional type

--- include/jclib/jclib.h
+++ include/jclib/jclib.h
@@ -5,6 +5,9 @@
 #define _jclib_h_
 
 
+#include <stdbool.h>
+
+
 #define offsetof(st, m) __builtin_offsetof(st, m)
 
 
@@ -27,4 +30,36 @@
     }
 
 
+/* Optionals --------------------------------------------------------------- */
+#define opt(_type) struct opt ## _type
+
+
+#define opt_typedef(_type) \
+opt(_type) { \
+    bool set; \
+    _type v; \
+}
+
+
+#define opt_value(_v) { true, (_v) }
+
+
+#define opt_none { .set = false }
+
+
+#define opt_is_set(_opt) _opt.set
+
+
+#define opt_get(_opt) \
+    ({ \
+        assert(_opt.set); \
+        _opt.v; \
+    })
+
+
+#define opt_set(_opt, _v) \
+    _opt = (typeof(_opt))opt_value(_v)
+
+
+/* ------------------------------------------------------------------------- */
 #endif
