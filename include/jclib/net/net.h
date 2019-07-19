b3a23af5f4c7e93dc279f384153fc83bdbaaa987
Jesus Christ <jesuschrist-official@protonmail.com>
2019-07-19 14:44:04 +0200

    net: start the net library

---
+++ include/jclib/net/net.h
@@ -0,0 +1,64 @@
+/*
+ * jclib/net/net.h - General networking related stuff.
+ */
+#ifndef _jclib_net_net_h_
+#define _jclib_net_net_h_
+
+
+typedef int socket_t;
+
+
+typedef enum net_status {
+    NET_ERROR = -1,
+    NET_SUCCESS = 0
+} net_status_t;
+
+
+#define SOCKET_ERROR    (-1)
+
+
+/*
+ * Set the given socket `sock` in non-blocking mode.
+ * Returns `NET_SUCESS` in case of success or `NET_ERROR` on failure.
+ */
+net_status_t socket_set_non_blocking(socket_t sock);
+
+
+/*
+ * Create a new TCP server socket listening on port `port` accepting
+ * `max_connections` connections.
+ * Returns the socket descriptor on success, or `SOCKET_ERROR` in case of
+ * failure.
+ */
+socket_t socket_create_server_tcp(int port, size_t max_connections);
+
+
+/*
+ * Create a new TCP client to the given `hostname` and `port`.
+ * Returns a valid socket in case of success or `NET_ERROR` otherwise.
+ */
+socket_t socket_create_client_tcp(const char* hostname, int port);
+
+
+/*
+ * Consume all data pendig on the socket input.
+ */
+void socket_flush(socket_t sock);
+
+
+/*
+ * Close the given socket.
+ */
+void socket_close(socket_t sock);
+
+
+/*
+ * Gently close the given socket:
+ * - call shutdown on the socket ;
+ * - flush the socket content ;
+ * - close the socket ;
+ */
+void socket_gently_close(socket_t sock);
+
+
+#endif
---
+++ src/net/net.c
@@ -0,0 +1,94 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include "jclib/net/net.h"
+
+
+net_status_t socket_set_non_blocking(socket_t sock) {
+    int flags = fcntl(sock, F_GETFL, 0);
+    if (fcntl(sock, F_SETFL, flags | O_NONBLOCK) < 0) {
+        return NET_ERROR;
+    }
+    return NET_SUCCESS;
+}
+
+
+socket_t socket_create_server_tcp(int port, size_t max_connections) {
+    socket_t sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR,
+                   &(int){ 1 }, sizeof(int)) < 0)
+    {
+        fprintf(stderr, "server socket will not be reusable\n");
+    }
+    struct sockaddr_in addr = {
+        .sin_addr.s_addr = htonl(INADDR_ANY),
+        .sin_family = AF_INET,
+        .sin_port = htons(port)
+    };
+
+    if (bind(sock,
+             (const struct sockaddr*)&addr,
+             sizeof(struct sockaddr_in))
+        < 0)
+    {
+        fprintf(stderr, "unable to bind listening socket on %d\n", port);
+        return SOCKET_ERROR;
+    }
+
+    if (listen(sock, max_connections) < 0) {
+        fprintf(stderr, "unable to listen for %zu connections\n",
+                max_connections);
+        return SOCKET_ERROR;
+    }
+
+    return sock;
+}
+
+
+socket_t socket_create_client_tcp(const char* hostname, int port) {
+    socket_t sock = socket(AF_INET, SOCK_STREAM, 0);
+    struct hostent* hostinfo;
+    struct sockaddr_in sin;
+
+    hostinfo = gethostbyname(hostname);
+    if (!hostinfo) {
+        fprintf(stderr, "unknown host %s\n", hostname);
+        return SOCKET_ERROR;
+    }
+
+    sin = (struct sockaddr_in){
+        .sin_addr = *(struct in_addr*)(hostinfo->h_addr_list[0]),
+        .sin_port = htons(port),
+        .sin_family = AF_INET
+    };
+    if (connect(sock, (struct sockaddr*)&sin, sizeof(struct sockaddr)) < 0) {
+        fprintf(stderr, "cannot connect to %s:%d\n", hostname, port);
+        return SOCKET_ERROR;
+    }
+
+    return sock;
+}
+
+
+void socket_flush(socket_t sock) {
+    char buf[4096];
+    while (recv(sock, buf, sizeof(buf), 0) > 0) {
+        /* nothing */
+    }
+}
+
+
+void socket_close(socket_t sock) {
+    close(sock);
+}
+
+
+void socket_gently_close(socket_t sock) {
+    shutdown(sock, SHUT_RD);
+    socket_flush(sock);
+    close(sock);
+}
